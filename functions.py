fb64fd2cabaef96ea3f5e33ff40b363259edee3c
Louis Popovic <louispop@gmail.com>
2018-10-18 13:23:18 -0400

    fonction read file progres

--- functions.py
+++ functions.py
@@ -1,6 +1,25 @@
-def my_function(word):
-    print(word)
-    other()
+BREAK_LINE = "\n"
+EMPTY_LINE = "\n\n"
 
-def other():
-    print("bonjour")
+def readFile(file):
+    
+    hasChargingDock = dict()
+    CLSC = dict()
+
+
+    f = open(file,"r").read()
+    
+    bornesCLSC,noeuds = f.split(EMPTY_LINE)
+
+    listBornes = bornesCLSC.split(BREAK_LINE)
+    listNoeuds = noeuds.split(BREAK_LINE)
+
+    for line in listBornes:
+        numero, hasCharge = line.split(',')
+        CLSC[numero] = hasCharge
+
+    for line in listNoeuds:
+        # remplir dictionnaire CLSC
+
+
+    return (hasChargingDock,CLSC)
--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-from functions import my_function
+from functions import *
+
+def test1():
+    my_dictionary = dict()
+    my_dictionary[1] = {2 : 85}
+    my_dictionary[1][3] = 217
+    my_dictionary[1][4] = 173
+
+    my_dictionary[2] = {5 : 80}
+    my_dictionary[2][6] = 56
+
+    print(my_dictionary)
 
 def main():
-    my_function("Hello World")
-    file = open("textTest.txt","r")
-    print (file.read())
+    CLSC = readFile("centresLocaux.txt")
+    #print(CLSC)
 
 main()
