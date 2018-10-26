f869e6a2e150c96c811cdecb44b255a3f9d9c7d2
Louis Popovic <louispop@gmail.com>
2018-10-26 12:19:02 -0400

    avancer dijk

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from graphesFunctions import creerGraphe, lireGraphe, plusCourtChemin
 
 GrapheCLSCs = None
 
+fakeDict = {
+    '1' : {'2':2,'4':3},
+    '2' : {'1':1,'3':5,'5':4},
+    '3' : {'2':2, '4':3},
+    '4' : {'1':3,'5':4},
+    '5' : {'2':4,'4':4}
+}
+
 def main():
     hasChargingDock, GrapheCLSCs = creerGraphe("centresLocaux.txt")
     #lireGraphe(GrapheCLSCs)
-    plusCourtChemin()
-    
+    plusCourtChemin(fakeDict)
 
+    
 main()
