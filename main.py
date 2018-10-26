40dd55ac1aa5131e7d7ad67c554405981e4cc08e
Louis Popovic <louispop@gmail.com>
2018-10-26 12:23:29 -0400

    sss

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from graphesFunctions import creerGraphe, lireGraphe, plusCourtChemin
-from menu import menu
 
 GrapheCLSCs = None
 
 fakeDict = {
-    '1' : {'2':2,'4':3},
+    '1' : {'2':1,'4':3},
     '2' : {'1':1,'3':5,'5':4},
     '3' : {'2':2, '4':3},
     '4' : {'1':3,'5':4},
@@ -12,17 +11,9 @@ fakeDict = {
 }
 
 def main():
-
-
-    #hasChargingDock, GrapheCLSCs = creerGraphe("centresLocaux.txt")
+    hasChargingDock, GrapheCLSCs = creerGraphe("centresLocaux.txt")
     #lireGraphe(GrapheCLSCs)
-<<<<<<< HEAD
     plusCourtChemin(fakeDict)
-=======
-    #plusCourtChemin()
-    menu()
-    
->>>>>>> efff31b686700044e6233e88b5cfa516c9bfb95c
 
     
 main()
