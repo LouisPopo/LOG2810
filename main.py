fcff622ffb97fa3f1fec193311af29c92d183c13
Félix-Antoine Bourbonnais <felix-atnoine.bourbonnais@polymtl.ca>
2018-10-29 12:53:50 -0400

    menu

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from graphesFunctions import *
 from menu import menu
 
-GrapheCLSCs = None
-
-fakeDict = {
-    'A' : {'B':1,'D':5},
-    'B' : {'A':1,'C':5,'E':1},
-    'C' : {'B':5, 'D':3},
-    'D' : {'A':3,'E':4},
-    'E' : {'B':4,'D':1}
-}
-
 def main():
-    hasChargingDock, GrapheCLSCs = creerGraphe("centresLocaux.txt")
-    result = plusCourtChemin('haut_risque','23','20')
-    print (result)
-    print(Vehicule.NINH)
+    menu()
     
     
 main()
