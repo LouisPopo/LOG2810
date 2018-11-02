2401372c37189aa636e1e117a498ae98e4ea8438
Félix-Antoine Bourbonnais <felix-atnoine.bourbonnais@polymtl.ca>
2018-11-01 22:33:31 -0400

    commit pcq merge problem

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ from graphesFunctions import *
 from menu import menu
 from dictionnaires import *
 
-GrapheCLSCs = None
-
-fakeDict = {
-    'A' : {'B':1,'D':5},
-    'B' : {'A':1,'C':5,'E':1},
-    'C' : {'B':5, 'D':3},
-    'D' : {'A':3,'E':4},
-    'E' : {'B':4,'D':1}
-}
-
 def main():
-    creerGraphe("centresLocaux.txt")
-    
-    a = testLongChemin('1', 50)
-    print(a)
+    menu()
 main()
